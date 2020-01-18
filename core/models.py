432e52f09bf3bfa5f6ed6b5d6b0f44e8bd0d229f
Cesar R <cesar.ramos77@gmail.com>
2020-01-18 15:07:35 -0300

    Adicionando retorno de linha em json e retorno Http004 e configuracao do time zone

--- core/models.py
+++ core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 # Create your models here.
+#O modo debug em Settings deve ser desailitado no modo em produção
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
@@ -19,3 +21,9 @@ class Evento(models.Model):
 
     def get_data_input_evento(self):
         return self.data_evento.strftime('%Y-%m-%dT%H:%M')
+
+    def get_evento_atrasado(self):
+        if self.data_evento <datetime.now():
+            return True
+        else:
+            return False
