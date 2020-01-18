672172fb463f1cf4e178ffbd0e03265df246c39d
Cesar R <cesar.ramos77@gmail.com>
2020-01-18 14:10:32 -0300

    Atualizando os dados da agenda

--- core/views.py
+++ core/views.py
@@ -39,7 +39,11 @@ def lista_eventos(request):
 
 @login_required(login_url='/login/')
 def evento(request):
-    return render(request, 'evento.html')
+    id_evento = request.GET.get('id')
+    dados = {} #dicionario vazio
+    if id_evento:
+        dados['evento'] = Evento.objects.get(id=id_evento)
+    return render(request, 'evento.html', dados)
 
 @login_required(login_url='/login/')
 def submit_evento(request):
@@ -48,9 +52,30 @@ def submit_evento(request):
         data_evento = request.POST.get('data_evento')
         descricao = request.POST.get('descricao')
         usuario = request.user
-        #registrando o evento
-        Evento.objects.create(titulo=titulo,
-                              data_evento=data_evento,
-                              descricao=descricao,
-                              usuario=usuario)
+        id_evento = request.POST.get('id_evento')
+        #editando um evento criado
+        if id_evento:
+            evento = Evento.objects.get(id=id_evento)
+            if evento.usuario == usuario:
+                evento.titulo = titulo
+                evento.descricao = descricao
+                evento.data_evento = data_evento
+                evento.save()
+            #Evento.objects.filter(id=id_evento).update(titulo=titulo,
+            #                                           data_evento=data_evento,
+            #                                           descricao=descricao)
+        else:
+            #registrando o evento se caso não existir
+            Evento.objects.create(titulo=titulo,
+                                  data_evento=data_evento,
+                                  descricao=descricao,
+                                  usuario=usuario)
+    return redirect('/')
+#deletando um registro
+@login_required(login_url='/login/')
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if usuario == evento.usuario:
+        evento.delete()
     return redirect('/')
